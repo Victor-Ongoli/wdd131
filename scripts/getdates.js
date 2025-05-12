912180ebf869c3ec37849630d916f2dffbc45da3
VictorOngoli <ongolivictor@gmail.com>
2025-05-12 17:59:25 +0300

    added emojis

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,5 @@
 const currentYear = new Date().getFullYear();
 const lastModified = document.lastModified;
 
-document.querySelector("#current-year").textContent = `c ${currentYear}`;
+document.querySelector("#current-year").textContent = `© ${currentYear}`;
 document.querySelector("#last-modified").textContent = `Last updated: ${lastModified}`;
