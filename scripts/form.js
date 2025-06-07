fe244e43409e4863d4316c3dd0c0b92edfeb127a
VictorOngoli <ongolivictor@gmail.com>
2025-06-08 01:10:57 +0300

    week 5 assignment

--- scripts/form.js
+++ scripts/form.js
@@ -25,3 +25,25 @@ const products = [
     averagerating: 5.0
   }
 ];
+
+const productSelect = document.getElementById('productSelect');
+
+function populateProducts() {
+  products.forEach(product => {
+    const option = document.createElement('option');
+
+    option.value = product.name;
+
+    option.textContent = product.name;
+
+    productSelect.appendChild(option);
+  });
+}
+
+populateProducts();
+
+const currentYear = new Date().getFullYear();
+const lastModified = document.lastModified;
+
+document.querySelector("#current-year").textContent = `© ${currentYear}`;
+document.querySelector("#last-modified").textContent = `Last updated: ${lastModified}`;
